3682896752ceb69bee59efec5a954e8e3cd1ab74
AROMAL S <aromalacs10@gmail.com>
2019-10-13 17:52:08 +0530

    Create thread2.c

---
+++ thread2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+int pizza = 200000;
+void* eat(void *a)
+{
+  int i;
+  for (i = 0; i < 20000; i++) {
+    pizza = pizza - 1;
+  }
+  return NULL;
+}
+
+int main()
+{
+  pthread_t threads[10];
+  int t;
+  printf("%i pizza on the table\n", pizza);
+  for (t = 0; t < 10; t++) {
+  	pthread_create(&threads[t],NULL,eat,NULL);
+  }
+  void* result;
+  for (t = 0; t < 10; t++) {
+  	pthread_join(threads[t], &result);
+  }
+  printf("There are now %i pizza on the table\n", pizza);
+  return 0;
+}
